d0c5c396160748be124500e5ac83b4f6caec2bff
Nathan Tranquilla <n1tranquilla@gmail.com>
2020-03-27 19:32:38 -0300

    add real appbar

--- src/app-bar/index.js
+++ src/app-bar/index.js
@@ -10,25 +10,71 @@ class AppBar extends HTMLElement {
         // adding a class to our container for the sake of clarity
         appBar.classList.add('root');
 
+        const navLinks = this.getAttribute('nav-links') || '';
+        const mult = this.getAttribute('mult') || 5
+
+        const linkStrings = navLinks.split(';') || [];
+
+        const linkPairs = linkStrings.map(function(str){ return str.split(':'); })
+        
+        const aTags = linkPairs.map(function(pair){
+            return `<a href="${pair[1]}">${pair[0]}</a>`
+        })
+
         // creating the inner HTML of the editable list element
         appBar.innerHTML = `
         <style>
            nav {
-               height: 2rem;
                font-family: var(--bdl-primary-font-family);
-               margin-top: calc(var(--bdl-spacing-unit) * 1rem);
-               margin-bottom: calc(var(--bdl-spacing-unit) * 2rem);
                padding: calc(var(--bdl-spacing-unit) * 1rem);
 
                display: flex;
-               justify-content: center;
+               justify-content: space-between;
                align-items: center;
+               flex-wrap: wrap;
 
-               background-color: var(--bdl-primary-color-light);
+               background-color: var(--bdl-primary-color);
+           }
+           #links {
+               height: 100%;
+               display: flex;
+               justify-content: space-between;
+               flex-grow: 1;
+               max-width: ${mult*linkPairs.length}rem;
+            }
+            #spacer {
+                width: ${mult/2}rem;
+            }
+            #logo-link {
+                border-bottom: none;
+            }
+            a {
+               margin: 0.25rem;
+               padding: 0.25rem;
+               padding-left: 0.75rem;
                color: var(--bdl-text-color);
-           } 
+               text-decoration: none;
+               text-transform: capitalize;
+               border-bottom: 2px solid transparent;
+               position: relative;
+            }
+            a:hover {
+                border-bottom: 2px solid var(--bdl-accent-color);
+            }
+            a:active:before {
+                content: "〉";
+                color: var(--bdl-accent-color);
+                position: absolute;
+                left: -0.5rem;
+                top:0;
+                font-weigh: bold;
+            } 
         </style>
-        <nav><slot></slot></nav>
+        <nav>
+           <a id="logo-link" href="${linkPairs[0][1]}"><slot></slot></a>
+           <div id="spacer"></div>
+           <div id="links">${aTags.join('')}</div>
+        </nav>
       `;
 
         // appending the container to the shadow DOM
--- stories/index.stories.js
+++ stories/index.stories.js
@@ -1,44 +1,17 @@
 import '../src/blog-dl.css'
-import '../src/hello-world'
 import '../src/app-bar'
-import '../src/root'
-import '../src/nav-link'
-import '../src/preview-card'
+import logo from '../assets/logo2.png'
 
 export default {
   title: 'Components',
 };
 
-const root = content => `<bdl-root>${content}</bdl-root>`
-const appBar = (...links) => {
-  return `<bdl-app-bar>${links}</bdl-app-bar>`
-}
-const navLink = text => `<bdl-nav-link>${text}</bdl-nav-link>`
-const previewCard = (title,text,timestamp) => `<bdl-preview-card title="${title}" text="${text}" timestamp="${timestamp}"></bdl-preview-card>`
-
-const text="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
-const title="Lorem Ipsum is simply dummy"
-
-export const IndexPage = () => {
-  const navLinks = navLink('Home')+navLink('Blog')+navLink('Other')
-  const content = appBar(navLinks)+previewCard(title,text,"2019-01-01")
-  return root(content)
+export const NavBar = () => {
+  return `<bdl-app-bar 
+    mult=5
+    nav-links="home:/index.html;about:/about.html;review:/review.html;contact:/contact.html">
+    <img src="${logo}" height="22" width="210" style="padding-top: 2.5px"/>
+  </bdl-app-bar>`
 }
 
-export const AppBar = () => {
-  return appBar();
-}
 
-export const NavLink = () => {
-  return root(`
-    <bdl-nav-link>Test</bdl-nav-link>
-    <bdl-nav-link active>Test</bdl-nav-link>
-  `)
-}
-
-export const PreviewCard = () => {
-  return root(`
-    <bdl-preview-card title="${title}" text="${text}" timestamp="2019-01-01"></bdl-preview-card>
-    <bdl-preview-card title="${title}" text="${text}" timestamp="2019-01-01"></bdl-preview-card>
-  `)
-}
