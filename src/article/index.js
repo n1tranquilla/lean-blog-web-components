22f354ebe0f60ab281cc3f7f5e1a9f359f579ff2
Nathan Tranquilla <n1tranquilla@gmail.com>
2020-04-09 06:27:29 -0300

    add tags to article

--- src/article/index.js
+++ src/article/index.js
@@ -1,90 +1,142 @@
-class Article extends HTMLElement {
-    constructor() {
-        super();
+(function () {
 
-        this.attachShadow({ mode: 'open' });
+    const convertTags = str =>str.split(';;')
+        .filter(s=>s.trim().length>0)
+        .map(row => {
+            const entries = row.split(';')
+            return entries.reduce((agg,entry)=>{
+                const [key,value] = entry.split(':')
+                agg[key] = value;
+                return agg;
+            },{})
+        })        
 
-        // creating a container for the editable-list component
-        const article = document.createElement('div');
+    const createTagsHtml = t=>`<a class="tag" href="${t.href}">${t.name}</a>`
 
-        // adding a class to our container for the sake of clarity
-        article.classList.add('root');
+    class Article extends HTMLElement {
 
-        const title = this.getAttribute('title') || '';
-        const date = this.getAttribute('date') || ''
+        
+        constructor() {
+            super();
 
-        // creating the inner HTML of the editable list element
-        article.innerHTML = `
-        <style>
-            article {
-                color: var(--lbwc-text-color);
-            }
-            #article-header {
-                width: 100%;
-                display: flex;
-                justify-content: space-between;
-                align-items: baseline;
-                flex-wrap: wrap;
-            }
-            h1 {
-                font-family: var(--lbwc-title-font-family);
-            }
-            span {
-                opacity: 0.75;
-                font-size: 0.7rem;
-            }
-        </style>
-        <article>
-            <div id="article-header">
-                <h1>${title}</h1>
-                <span>${date}</span>
-            </div>
-            <slot></slot>
-        </article>
-      `;
+            this.attachShadow({ mode: 'open' });
 
-        // appending the container to the shadow DOM
-        this.shadowRoot.appendChild(article);
+            // creating a container for the editable-list component
+            const article = document.createElement('div');
 
-        this.titleEl = this.shadowRoot.getElementById("article-header").children[0];
-        this.dateEl = this.shadowRoot.getElementById("article-header").children[1];
-    }
+            // adding a class to our container for the sake of clarity
+            article.classList.add('root');
 
-    get title(){
-        return this.getAttribute('title') || '';
-    }
+            const title = this.getAttribute('title') || '';
+            const date = this.getAttribute('date') || ''
+            const tags = convertTags(this.getAttribute('tags') || '');
 
-    set title(value) {
-        this.setAttribute('title',value)
-    }
+            // creating the inner HTML of the editable list element
+            article.innerHTML = `
+                <style>
+                    article {
+                        color: var(--lbwc-text-color);
+                    }
+                    #article-header {
+                        width: 100%;
+                        display: flex;
+                        justify-content: flex-start;
+                        align-items: baseline;
+                        flex-wrap: wrap;
+                    }
+                    #article-subheader {
+                        width: 100%;
+                        display:flex;
+                        justify-content: space-between;
+                        align-items: baseline;
+                    }
+                    h1 {
+                        font-family: var(--lbwc-title-font-family);
+                    }
+                    #date {
+                        opacity: 0.75;
+                        font-size: 0.7rem;
+                    }
+                    .tag {
+                        font-size: 0.7rem;
+                        padding: 3px 10px;
+                        background-color: #f3f3f3;
+                        border-radius: 2px;
+                        margin: 0 0.25rem;
+                        color: var(--lbwc-accent-color);
+                    }
+                </style>
+                <article>
+                    <div id="article-header">
+                        <h1>${title}</h1>
+                    </div>
+                    <div id="article-subheader">
+                        <span id="date">${date}</span>
+                        <span>${tags.map(createTagsHtml).join('')}</span>
+                    </div>
+                    <slot></slot>
+                </article>
+                `;
 
-    get date() {
-        return this.getAttribute('date') || ''
-    }
+            // appending the container to the shadow DOM
+            this.shadowRoot.appendChild(article);
 
-    set date(value) {
-        this.setAttribute('date',value)
-    }
+            this.titleEl = this.shadowRoot.getElementById("article-header").children[0];
+            this.dateEl = this.shadowRoot.getElementById("article-subheader").children[0];
+            this.tagsEl = this.shadowRoot.getElementById("article-subheader").children[1];
 
-    static get observedAttributes() {
-        return ['title', 'date'];
-    }
+        }
 
-    attributeChangedCallback(name, oldValue, newValue) {
-        switch (name) {
-            case 'title': {
-                this.titleEl.innerHTML = newValue;
-                break;
-            }
-            case 'date': {
-                this.dateEl.innerHTML = newValue;
-                break;
-            }
-            default: {
-                break;
+        get title() {
+            return this.getAttribute('title') || '';
+        }
+
+        set title(value) {
+            this.setAttribute('title', value)
+        }
+
+        get tags() {
+            return this.getAttribute('tags') || [];
+        }
+
+        set tags(value) {
+            this.setAttribute('tags', value);
+        }
+
+        get date() {
+            return this.getAttribute('date') || ''
+        }
+
+        set date(value) {
+            this.setAttribute('date', value)
+        }
+
+        static get observedAttributes() {
+            return ['title', 'date'];
+        }
+
+        attributeChangedCallback(name, oldValue, newValue) {
+            switch (name) {
+                case 'title': {
+                    this.titleEl.innerHTML = newValue;
+                    break;
+                }
+                case 'date': {
+                    this.dateEl.innerHTML = newValue;
+                    break;
+                }
+                case 'tags': {
+                    const tags = convertTags(newValue);
+                    this.tagsEl.innerHTML = tags.map(createTagsHtml);
+                    break;
+                }
+                default: {
+                    break;
+                }
             }
         }
     }
-}
 
-customElements.get('lbwc-article') || customElements.define('lbwc-article', Article);
+    customElements.get('lbwc-article') || customElements.define('lbwc-article', Article);
+
+})()
